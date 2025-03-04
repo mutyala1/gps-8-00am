d0a2801d6b46c129ed0855ed5ac1f4d095cd3bf2
dev1 <dev1@gmail.com>
2025-03-04 08:28:25 +0530

    modified by dev2

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
-Class Hello
+Class Hello#dev2
   {
-	  Public static void main (String args[])
+	  Public static void main (String args[])#dev2
 		   {
 			   System.out.ptintln("Hello world");
 		    }
-   }
+   }#dev2
